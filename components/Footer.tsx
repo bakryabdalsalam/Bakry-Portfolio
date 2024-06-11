eb44c7ca62b6ef085a5833c2fe200676dcd0749e
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2024-06-11 12:10:36 +0300

    refactor: Add target="_blank" to social media links in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,14 +30,16 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
-          ))}
-        </div>
+    <div
+      key={info.id}
+      className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+    >
+      <a href={info.url} target="_blank" rel="noopener noreferrer">
+        <img src={info.img} alt="icons" width={20} height={20} />
+      </a>
+    </div>
+  ))}
+</div>
       </div>
     </footer>
   );
