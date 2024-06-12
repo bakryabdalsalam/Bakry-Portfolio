b15e9319f6068b81a98a32fa5bf794ac4a77bf93
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2024-06-12 12:32:57 +0300

    chore: Update social media links in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -24,22 +23,22 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
+        <p className="md:text-base text-sm md:font-normal font-light mb-4 md:mb-0">
           Copyright © 2024 Bakry Abdelsalam
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-    <div
-      key={info.id}
-      className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-    >
-      <a href={info.url} target="_blank" rel="noopener noreferrer">
-        <img src={info.img} alt="icons" width={20} height={20} />
-      </a>
-    </div>
-  ))}
-</div>
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <a href={info.url} target="_blank" rel="noopener noreferrer">
+                <img src={info.img} alt="icons" width={20} height={20} />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
